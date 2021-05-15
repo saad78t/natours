9b2bd88ea8a6979814166f63b0a67092d97d5347
SaadTurky <Saadstj@gmail.com>
2021-05-15 13:55:11 +0300

    adding alert

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alert';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
@@ -62,3 +63,6 @@ if (bookbtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
